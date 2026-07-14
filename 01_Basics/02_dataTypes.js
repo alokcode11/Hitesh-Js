547475d75daa7e61c0f1c0b690c8dbd025974d52
alokcode11 <alokbabu911@gmail.com>
2026-07-15 03:10:47 +0530

    Add data types demonstration in 02_dataTypes.js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // Treat all js code as newer version of the js 
+
+// alert("Hello")  // we are using node js not browser 
+// console.log("Alok")  console.log("Abhi");
+console.log("Alok"); console.log("Abhi");
+
+let name = "alok" // string dataType
+let age = 20 // Number dataType
+let isLoggedIn = false // Boolean dataType 
+
+/*
+number -> Range varies till 2^53
+BigInt -> specially for trading
+string => single or double quote
+boolean -> yes or no
+null    -> Is is actually a stand alone value as well data type | Representation of the empty value [like what is temp and u dont know then u cant say it 0 u say null mean khali]
+undefined -> value not assigned 
+symbol -> It is unique and mostly used in the react like this component is unique [used to find the uniqueness]
+*/
+
+// Object 
+
+console.log(typeof age); // number
+console.log(typeof null);// object
+console.log(typeof undefined); // undefined 
+
+
+
+
