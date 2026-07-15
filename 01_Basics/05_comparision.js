09a5ed3ad466333d40f5331011166527763e54d8
alokcode11 <alokbabu911@gmail.com>
2026-07-15 16:35:15 +0530

    Add comparison operations demonstration in 05_comparision.js

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1); // true 
+// console.log(2 <= 1); // false 
+// console.log(2 == 1); // false  
+// console.log(2 != 1); // true 
+
+// console.log("2" > 1); // true Here it auto convert 2 into no 
+// console.log("02" > 1); // true 
+
+
+// console.log(null > 0); // Here comparision so 0 > 0 is  ||  false 
+// console.log(null == 0);// false || It works diff than the >, < , >= , etc.. it directly match the value  || Follow special equality rules 
+// console.log(null >= 0);// here convert then comparision  || true
+// console.log(null == undefined); // user ne koi value di ni hai and js ne koi value ni assign ki hai so theu are equal || true 
+
+
+// console.log(undefined < 0); //false 
+// console.log(undefined == 0); //false 
+
+console.log(2 == "2");
+console.log(2 === "2"); // No conversion check data tyoe first 
+
+
+
+
+
+
+
+
+
