2fd98a29c219b0d3daaa62b5393b5a110e600f5c
alokcode11 <alokbabu911@gmail.com>
2026-07-26 20:36:32 +0530

    do while loop added

---
+++ 05_Iterations/02_doWhileLoops.js
@@ -0,0 +1,23 @@
+// while
+
+let index = 0 // Inialization
+while (index < 10) { // Condition check 
+    console.log(`value of index is ${index}`);
+    index = index + 2; //updation 
+}
+
+let arr = ['a', 'b', 'c', 'd']
+let i = 0
+while (i < arr.length) {
+    console.log(`value is ${arr[i]}`);
+    i++
+}
+
+// do while loop 
+// atleast ek baar to execute hoga 
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while(score < 10) // 11 print 
